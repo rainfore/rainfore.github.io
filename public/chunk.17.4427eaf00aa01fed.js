a95c7fa9e47d90105fe64019112f3ad1210fb6ad
Rainfore <rainforest92@126.com>
2019-01-14 22:27:13 +0800

    :rocket: Add public

---
+++ public/chunk.17.4427eaf00aa01fed.js
@@ -0,0 +1,1 @@
+webpackJsonp([17],{"6YFc":function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement,t=this._self._c||e;return t("u-article",[t("router-view")],1)},staticRenderFns:[]},l=n("OLXp")(null,r,!1,null,null,null);t.default=l.exports}});
